4481c16865087177a9453ad4ea0c750832647345
HyperNeutron <nigel.koremans@gmail.com>
2025-04-08 09:40:06 +0200

    added dbconnection class

--- NSdisplay/MainWindow.xaml.cs
+++ NSdisplay/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Data.Common;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,13 +9,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace NSdisplay
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public class DBConnection
+    {
+        private DBConnection()
+    {
+    }
+
+    public string Server { get; set; }
+    public string DatabaseName { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public MySqlConnection Connection { get; set; }
+
+    private static DBConnection _instance = null;
+    public static DBConnection Instance()
+    {
+        if (_instance == null)
+            _instance = new DBConnection();
+        return _instance;
+    }
+
+    public bool IsConnect()
+    {
+        if (Connection == null)
+        {
+            if (String.IsNullOrEmpty(DatabaseName))
+                return false;
+            string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
+            Connection = new MySqlConnection(connstring);
+            Connection.Open();
+        }
+
+        return true;
+    }
+
+    public void Close()
+    {
+        Connection.Close();
+    }
+}
+public partial class MainWindow : Window
     {
         public MainWindow()
         {
