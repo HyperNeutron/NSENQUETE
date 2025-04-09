2556720bc220318fb7c16f874c801a246cc630b2
Jason <jasonvanl2019@gmail.com>
2025-04-09 08:46:15 +0200

    Added a Mysql Conn connection

---
+++ NSdisplay/services/Database/Config.example.cs
@@ -0,0 +1,12 @@
+﻿namespace NSdisplay.services.Database;
+
+public class ConfigExample
+{
+    public static string Host { get; set; } = "localhost";
+
+    public static string Name { get; set; } = "NS";
+
+    public static string User { get; set; } = "root";
+
+    public static string Pass { get; set; } = "";
+}
