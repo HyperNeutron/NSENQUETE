13c184dadc4c5bf286bd28ecaf1a3748f0a7fd82
HyperNeutron <nigel.koremans@gmail.com>
2025-04-09 09:21:01 +0200

    display station name from id parameter

--- NSdisplay/MainWindow.xaml.cs
+++ NSdisplay/MainWindow.xaml.cs
@@ -20,48 +20,68 @@ namespace NSdisplay
     public class DBConnection
     {
         private DBConnection()
-    {
-    }
-
-    public string Server { get; set; }
-    public string DatabaseName { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
-
-    public MySqlConnection Connection { get; set; }
-
-    private static DBConnection _instance = null;
-    public static DBConnection Instance()
-    {
-        if (_instance == null)
-            _instance = new DBConnection();
-        return _instance;
-    }
-
-    public bool IsConnect()
-    {
-        if (Connection == null)
         {
-            if (String.IsNullOrEmpty(DatabaseName))
-                return false;
-            string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
-            Connection = new MySqlConnection(connstring);
-            Connection.Open();
         }
 
-        return true;
-    }
+        public string Server { get; set; }
+        public string DatabaseName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
 
-    public void Close()
-    {
-        Connection.Close();
-    }
-}
-public partial class MainWindow : Window
-    {
-        public MainWindow()
+        public MySqlConnection Connection { get; set; }
+
+        private static DBConnection _instance = null;
+        public static DBConnection Instance()
         {
+            if (_instance == null)
+                _instance = new DBConnection();
+            return _instance;
+        }
+
+        public bool IsConnect()
+        {
+            if (Connection == null)
+            {
+                if (String.IsNullOrEmpty(DatabaseName))
+                    return false;
+                string connstring = string.Format("Server={0}; database={1}; UID={2}; password={3}", Server, DatabaseName, UserName, Password);
+                Connection = new MySqlConnection(connstring);
+                Connection.Open();
+            }
+
+            return true;
+        }
+
+        public void Close()
+        {
+            Connection.Close();
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public static DBConnection DBcon = DBConnection.Instance();
+        public MainWindow(int id)
+        {
+            int stationID = id;
+            string stationName;
+            DBcon.Server = "localhost";
+            DBcon.DatabaseName = "ns";
+            DBcon.UserName = "root";
             InitializeComponent();
+            if (DBcon.IsConnect())
+            {
+                string query = "SELECT id, name FROM netherlands_train_stations WHERE id = @stationID";
+                var cmd = new MySqlCommand(query, DBcon.Connection);
+                cmd.Parameters.AddWithValue("@stationID", stationID);
+                var reader = cmd.ExecuteReader();
+                station.Text = "bbb";
+                if (reader.Read())
+                {
+                    stationName = reader.GetString(1);
+                    station.Text = stationName;
+                }
+                reader.Close();
+            }
         }
     }
 }
