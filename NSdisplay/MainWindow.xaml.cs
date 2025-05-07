ec072e3a22a329c511bfca044a281e3949419a51
HyperNeutron <nigel.koremans@gmail.com>
2025-05-07 10:27:33 +0200

    added doublewidth converter

--- NSdisplay/MainWindow.xaml.cs
+++ NSdisplay/MainWindow.xaml.cs
@@ -90,7 +90,16 @@ namespace NSdisplay
             }
         }
     }
+    public class DoubleWidth : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (double)value * 2;
+        }
 
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
