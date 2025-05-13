e1d11ecc6d67e5f2d82c7bf1960663dc7601976f
HyperNeutron <nigel.koremans@gmail.com>
2025-05-13 09:39:24 +0200

    added fetching feedback from database

--- NSdisplay/MainWindow.xaml.cs
+++ NSdisplay/MainWindow.xaml.cs
@@ -23,13 +23,15 @@ namespace NSdisplay
     public partial class MainWindow : Window
     {
         public static MySqlConnection con = DbConnection.GetConnection();
+        int stationID;
+
         public MainWindow(int id)
         {
             InitializeComponent();
 
             con.Open();
 
-            int stationID = id;
+            stationID = id;
             string stationName;
 
             string query = "SELECT name, hasLift, wheelChairAccessible, hasToilet, haskiosk FROM netherlands_train_stations WHERE id = @stationID";
@@ -65,6 +67,7 @@ namespace NSdisplay
                 }
             }
             reader.Close();
+
             updateClock();
             nextReview();
         }
@@ -80,14 +83,63 @@ namespace NSdisplay
 
         async void nextReview()
         {
-            await Task.Delay(TimeSpan.FromSeconds(1));
+            int currentFeedback = 0;
+            int nextFeedback = 0;
+            int feedbackAmount = 5;
+
+            //Without this the animation wont work 
+            await Task.Delay(TimeSpan.FromMilliseconds(1));
+
+            approvedFeedback[] latestFeedback = new approvedFeedback[feedbackAmount];
+
+            //placeholder feedback
+            latestFeedback[0] = new approvedFeedback("", "Geen feedback gevonden", new DateTime(0));
+
             double width = reviewContainer.ActualWidth;
-            ThicknessAnimation reviewAnimation = new ThicknessAnimation(new Thickness(0, 0, 0, 20), new Thickness(-width/2,0,0,20), TimeSpan.FromSeconds(1));
-            reviewAnimation.EasingFunction = new SineEase();
-            while (true)
+            ThicknessAnimation reviewAnimation = new(new Thickness(0, 0, 0, 20), new Thickness(-width / 2, 0, 0, 20), TimeSpan.FromSeconds(1))
             {
+                EasingFunction = new SineEase()
+            };
+
+            while (true) {
+                string query = "SELECT sender, shortMessage, date_posted from messages WHERE is_approved AND station_id = @station_id ORDER BY date_posted DESC LIMIT @feedbackAmount";
+                var cmd = new MySqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@feedbackAmount", feedbackAmount);
+                cmd.Parameters.AddWithValue("@station_id", stationID);
+                var reader = cmd.ExecuteReader();
+
+                int count = 0;
+                while (reader.Read())
+                {
+                    latestFeedback[count] = new approvedFeedback(reader.GetString(0), reader.GetString(1), reader.GetDateTime(2));
+                    count++;
+                }
+
+                reader.Close();
+
+                reviewContent1.Text = latestFeedback[currentFeedback].small_story;
+                reviewer1.Text = latestFeedback[currentFeedback].name;
+                date1.Text = latestFeedback[currentFeedback].date_posted.ToString("dd/MM");
+
+                if (count <= 1)
+                {
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    continue;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(3));
+
                 reviewContainer.BeginAnimation(MarginProperty, reviewAnimation);
-                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                currentFeedback = nextFeedback;
+
+                nextFeedback += nextFeedback >= count - 1 ? -(count - 1) : 1;
+
+                reviewContent2.Text = latestFeedback[nextFeedback].small_story;
+                reviewer2.Text = latestFeedback[nextFeedback].name;
+                date2.Text = latestFeedback[nextFeedback].date_posted.ToString("dd/MM");
+
+                await Task.Delay(TimeSpan.FromSeconds(3));
             }
         }
     }
@@ -101,6 +153,20 @@ namespace NSdisplay
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+    }
+
+    public class approvedFeedback
+    {
+        public string name;
+        public string small_story;
+        public DateTime date_posted;
+
+        public approvedFeedback(string name, string small_story, DateTime approved_on)
+        {
+            this.name = name;
+            this.small_story = small_story;
+            this.date_posted = approved_on;
         }
     }
 }
